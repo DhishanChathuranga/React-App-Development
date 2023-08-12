30627689e635162eedbbc23dfbb46e2d25971712
Dhishan Chathuranga <dhishanc.creation@gmail.com>
2023-08-12 23:15:37 +0530

    Create Compornents

---
+++ src/Components/Main.js
@@ -0,0 +1,13 @@
+function Main(props){
+    return(
+        <div>
+            {props.children}
+            This is Main Components
+            <p>
+                My name is Dhishan Chathuranga. I am 29 years old boy. I am freelance designer in 2020. 
+            </p>
+        </div>
+    );
+}
+
+export default Main
